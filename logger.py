42d53120982e808389fe994b829656f6a99c40f7
slmoore <slmoore@node0.slmoore-270187.bayopsys-pg0.wisc.cloudlab.us>
2025-09-16 12:51:08 -0500

    Logging is now continuous

--- logger.py
+++ logger.py
@@ -3,27 +3,30 @@
 import logging
 import subprocess
 
+LOG_FILE_PATH = "power_consumption_info.log"
+
 
 def main() -> None:
     logging.basicConfig(
-            filename="info.log",
-            filemode='w',
+            filename=LOG_FILE_PATH,
+            filemode='a',
             format='%(asctime)s %(message)s',
             level=logging.INFO)
 
     logger = logging.getLogger()
-
-    try:
-        result = subprocess.run(
-            "sudo ipmitool sensor | grep Watts",
-            shell=True,
-            capture_output=True,
-            check=True,
-            text=True)
+    
+    while True:
+        try:
+            result = subprocess.run(
+                "sudo ipmitool sensor | grep Watts",
+                shell=True,
+                capture_output=True,
+                check=True,
+                text=True)
         
-        logger.info(f"\n{result.stdout}\n")
-    except Exception as err:
-        logger.error(err.stderr)
+            logger.info(f"\n{result.stdout}")
+        except Exception as err:
+            logger.error(err.stderr)
 
 
 if __name__ == '__main__':
