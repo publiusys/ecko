788433bf1a9fee7ecd3d2bee79f26fb8b713d518
hand32 <hand32@node0.hand32-270403.bayopsys-pg0.wisc.cloudlab.us>
2025-09-17 10:51:59 -0500

    example perf with file logging capability

--- microbenchmarks/stress-ng/perfsensor.sh
+++ microbenchmarks/stress-ng/perfsensor.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
 
+# to run: ./perfsensor.sh loop
 
-perf stat -a -e power/energy-pkg/ -x, -I 1000
+currdate=$(date +%m%d%Y%H%M%S)
+
+function orig()
+{
+    perf stat -a -e power/energy-pkg/ -x, -I 1000
+}
+
+function loop()
+{
+    perf stat -a -e instructions,cache-misses,ref-cycles,power/energy-pkg/,power/energy-ram/ -x, -I 1000 -o "perf.${currdate}.log"
+}
+
+$@
