97c1c49d2d5d82fa0730403e4310192407a0da7b
slmoore <slmoore@node0.slmoore-271515.bayopsys-pg0.wisc.cloudlab.us>
2025-09-24 11:11:16 -0500

    Implemented logging for perf sensor

--- microbenchmarks/stress-ng/logger.py
+++ microbenchmarks/stress-ng/logger.py
@@ -11,23 +11,47 @@ async def log_ipmisensor(logger : Logger) -> None:
     """Continuously log the output of ipmitool sensor"""
     while True:
         try:
+            cmd = "sudo ipmitool sensor"
+
             proc = await asyncio.create_subprocess_shell(
-                    "sudo ipmitool sensor",
-                     stdout=asyncio.subprocess.PIPE,
-                     stderr=asyncio.subprocess.PIPE)
+                    cmd,
+                    stdout=asyncio.subprocess.PIPE,
+                    stderr=asyncio.subprocess.PIPE)
 
             stdout, stderr = await proc.communicate()
 
-            for line in stdout.splitlines():
+            for line in stdout.decode().splitlines():
                 if "Watts" in line:
                     logger.info(','.join([part.strip() for part in line.split("|")]))
         except Exception as err:
             logger.error(err)
 
 
-#async def log_perfsensor(logger : Logger) -> None:
-#    """Continuously log the output of perf sensor"""
-#    pass
+async def log_perfsensor(logger : Logger) -> None:
+    """Continuously log the output of perf sensor"""
+    cmd = (
+        "sudo perf stat -a "
+        "-e instructions,cache-misses,ref-cycles,power/energy-pkg/,power/energy-ram/ "
+        "-x, -I 1000")
+
+    proc = await asyncio.create_subprocess_shell(
+        cmd,
+        stdout=asyncio.subprocess.PIPE,
+        stderr=asyncio.subprocess.PIPE)
+
+    try:
+        while True:
+            line = await proc.stderr.readline()
+
+            if not line:
+                break
+
+            line_text = line.decode().strip()
+
+            if line_text:
+                logger.info(line_text)
+    except Exception as err:
+        logger.error(err)
 
 
 async def async_main() -> None:
@@ -41,7 +65,8 @@ async def async_main() -> None:
     logger = logging.getLogger()
 
     await asyncio.gather(
-            log_ipmisensor(logger))
+            log_ipmisensor(logger),
+            log_perfsensor(logger))
 
 
 def main() -> None:
