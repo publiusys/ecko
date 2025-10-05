32d6acb260de77cb9409141a7eb74e763ec4f5a8
Sef <155701785+m-sef@users.noreply.github.com>
2025-10-05 17:39:06 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+# usage: setup node_name
+
+node_name=$1
 
 sudo apt-get update -y
 sudo apt-get install -y ipmitool
+sudo apt-get install -y docker.io
+sudo apt-get install -y kubelet kubeadm kubectl
+
+if [ node_name == "node0" ]; then
+  touch master.txt
+fi
