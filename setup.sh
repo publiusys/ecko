c25aec93435816d1df33b59a0ccbe6ba7494208a
Sef <155701785+m-sef@users.noreply.github.com>
2025-10-08 17:02:39 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -9,3 +9,16 @@ sudo swapoff -a
 
 sudo modprobe overlay
 sudo modprobe br_netfilter
+
+sudo tee /etc/modules-load.d/k8s.conf <<EOF
+overlay
+br_netfilter
+EOF
+
+sudo tee /etc/sysctl.d/k8s.conf << EOF
+net.bridge.bridge-nf-call-iptables  = 1
+net.bridge.bridge-nf-call-ip6tables = 1
+net.ipv4.ip_forward   = 1
+EOF
+
+sudo sysctl --system
