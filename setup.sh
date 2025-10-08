4aa7c119c781a364b7378fbc93e3b22db084880f
Sef <155701785+m-sef@users.noreply.github.com>
2025-10-08 17:00:40 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
-# usage: setup node_name
-
-node_name=$1
 
 sudo apt-get update -y
 sudo apt-get install -y ipmitool
 sudo apt-get install -y docker.io
 sudo apt-get install -y kubelet kubeadm kubectl
 
-if [ node_name == "node0" ]; then
-  touch master.txt
-fi
+sudo swapoff -a
+
+sudo modprobe overlay
+sudo modprobe br_netfilter
