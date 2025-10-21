48f8ad81b96efbd29ece5bca6d9992cee577592a
Sef <155701785+m-sef@users.noreply.github.com>
2025-10-21 09:53:37 -0400

    Fix installation command for Kubernetes packages

--- setup.sh
+++ setup.sh
@@ -36,5 +36,5 @@ sudo systemctl restart containerd.service
 sudo apt-get install curl ca-certificates apt-transport-https  -y
 curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.31/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
 echo "deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.31/deb/ /" | sudo tee /etc/apt/sources.list.d/kubernetes.list
-sudo apt-get install -y kubelet kubeadm kubectl
 sudo apt update
+sudo apt-get install -y kubelet kubeadm kubectl
