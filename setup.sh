b7f89b79c159b1e4740ad9acbc7cdbd1a30db188
Sef <155701785+m-sef@users.noreply.github.com>
2025-10-23 10:07:11 -0400

    Enhance setup.sh for Kubernetes master and worker setup

--- setup.sh
+++ setup.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+if [[ "$(hostname -I)" == *"10.10.1.1"* ]]; then
+  IS_MASTER=true
+else
+  IS_MASTER=false
+fi
+
 sudo apt-get update -y
 sudo apt-get install -y ipmitool
 sudo apt-get install -y docker.io
@@ -38,3 +44,23 @@ curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.31/deb/Release.key | sudo gpg --
 echo "deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.31/deb/ /" | sudo tee /etc/apt/sources.list.d/kubernetes.list
 sudo apt update
 sudo apt-get install -y kubelet kubeadm kubectl
+
+if [ "$IS_MASTER" = true ]; then
+  sudo kubeadm init --pod-network-cidr=10.10.0.0/16
+  
+  mkdir -p $HOME/.kube
+  sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+  sudo chown $(id -u):$(id -g) $HOME/.kube/config
+  
+  kubectl create -f https://raw.githubusercontent.com/projectcalico/calico/v3.28.0/manifests/tigera-operator.yaml
+  curl https://raw.githubusercontent.com/projectcalico/calico/v3.28.0/manifests/custom-resources.yaml -O
+  sed -i 's/cidr: 192\.168\.0\.0\/16/cidr: 10.10.0.0\/16/g' custom-resources.yaml
+  kubectl create -f custom-resources.yaml
+else
+  until ssh -o StrictHostKeyChecking=no node0 "kubectl get nodes >/dev/null 2>&1"; do
+    sleep 5
+  done
+
+  JOIN_CMD=$(ssh -o StrictHostKeyChecking=no node0 "sudo kubeadm token create --print-join-command 2>/dev/null")
+  sudo $JOIN_CMD
+fi
