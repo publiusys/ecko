6f3520f5ae2373211fa92b4244d0f47e3e81c188
Sef <155701785+m-sef@users.noreply.github.com>
2025-11-06 08:14:44 -0500

    Clean up comments in is-master.sh
    Removed author and last modified comments from is-master.sh.

--- is-master.sh
+++ is-master.sh
@@ -5,10 +5,6 @@
 #	Checks if the node this script is executed on should be initialized as the master node.
 #	Prints "MASTER" if the node's IP is 10.1.1.1, and returns exit code 0.
 #	Returns exit code 1 otherwise.
-#
-# Author: Seth Moore (slmoore@hamilton.edu)
-#
-# Last Modified: 10/28/2025
 
 if [[ "$(hostname -s)" == "client" ]]; then
 	echo "MASTER"
