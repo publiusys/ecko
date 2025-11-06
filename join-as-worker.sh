77d0c3b3175db33da31968848529d754a999c3b5
slmoore <slmoore@worker0.slmoore-277938.bayopsys-pg0.wisc.cloudlab.us>
2025-11-06 08:23:45 -0600

    Modified join-as-worker to reflect new client node name

--- join-as-worker.sh
+++ join-as-worker.sh
@@ -5,10 +5,10 @@
 # If the node this script is run on is not the master node, then it will attempt to join the kubernetes cluster.
 
 if ! ./is-master.sh; then
-	until ssh -o StrictHostKeyChecking=no node0 "kubectl get nodes >/dev/null 2>&1"; do
+	until ssh -o StrictHostKeyChecking=no client "kubectl get nodes >/dev/null 2>&1"; do
 		sleep 5
 	done
 
-	JOIN_CMD=$(ssh -o StrictHostKeyChecking=no node0 "sudo kubeadm token create --print-join-command 2>/dev/null")
+	JOIN_CMD=$(ssh -o StrictHostKeyChecking=no client "sudo kubeadm token create --print-join-command 2>/dev/null")
 	sudo $JOIN_CMD
 fi
