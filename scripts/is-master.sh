21d6c4589f47a27d149c81a40301fcc1a698039b
hand32 <hand32@client1.hand32-281413.bayopsys-pg0.wisc.cloudlab.us>
2025-11-24 13:42:42 -0600

    changes is-master.sh to check if client is a substring of hostname -s

--- scripts/is-master.sh
+++ scripts/is-master.sh
@@ -3,10 +3,10 @@
 # 
 # Description:
 #	Checks if the node this script is executed on should be initialized as the master node.
-#	Prints "MASTER" if the node's IP is 10.1.1.1, and returns exit code 0.
+#	Prints "MASTER" if the node's hostname contains "client", and returns exit code 0.
 #	Returns exit code 1 otherwise.
 
-if [[ "$(hostname -s)" == "client" ]]; then
+if [[ "$(hostname -s)" == *"client"* ]]; then
 	echo "MASTER"
 	exit 0
 else
