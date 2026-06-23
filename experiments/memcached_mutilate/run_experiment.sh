e875a9d3dca2b98262d4862d917e2aea20c2ae73
m-sef <seth.l.moore0506@gmail.com>
2026-06-23 12:53:14 -0400

    Modified run_experiment.sh

--- experiments/memcached_mutilate/run_experiment.sh
+++ experiments/memcached_mutilate/run_experiment.sh
@@ -3,38 +3,42 @@
 #            Seth Moore (slmoore@hamilton.edu)
 # Brief: 
 
-port=11211
-server=10.10.1.1
-agent1=10.10.1.2
-agent2=10.10.1.3
+PORT=11211
+AGENT1=10.10.1.2
+AGENT2=10.10.1.3
 
 run_experiment() {
         echo "START MEMCACHED 16 THREADS"
-        ssh $server "taskset -c 0-15 memcached -p $port -u nobody -t 16 -m 32G -c 8192 -b 8192 -l $server -B binary > mcd_11211.log 2>&1 < /dev/null &"
+        #ssh $server "taskset -c 0-15 memcached -p $PORT -u nobody -t 16 -m 32G -c 8192 -b 8192 -l $SERVER -B binary > mcd_11211.log 2>&1 < /dev/null &"
+        sudo kubectl apply -f yaml/memcached.yaml
+        sudo kubectl wait --for=condition=Ready pod -l app=memcached -n memcached --timeout=120s
+
+        SERVER="http://$(sudo kubectl get svc memcached-service -n memcached -o jsonpath='{.spec.clusterIP}')"
 
         echo "START LOAD GENERATION AGENTS"
         ssh $agent1 "mutilate --agentmode --threads=16 > agent.log 2>&1 < /dev/null &"
         ssh $agent2 "mutilate --agentmode --threads=16 > agent.log 2>&1 < /dev/null &"
 
         echo "LOAD MEMCACHED DATABASE"
-        taskset -c 0 mutilate -vv --binary -s $server:$port --loadonly -K fb_key -V fb_value
+        taskset -c 0 mutilate -vv --binary -s $SERVER:$PORT --loadonly -K fb_key -V fb_value
 
         sleep 1
 
-        update=$1
-        qps=$2
+        UPDATE=$1
+        QPS=$2
 
-        echo "START RUN UPDATE $update QPS $qps"
-        mkdir -p exp/$update\_$qps/
+        echo "START RUN UPDATE $UPDATE QPS $QPS"
+        mkdir -p exp/$UPDATE\_$QPS/
 
         # start load generation for 1 mcd process
-        taskset -c 0 mutilate --binary -s $server:$port --noload --agent={$agent1,$agent2} --threads=1 --keysize=fb_key --valuesize=fb_value --iadist=fb_ia --update=0.25 --depth=128 --measure_connections=32 --qps=$qps --time=30 >> exp/$update\_$qps/leader.log
+        taskset -c 0 mutilate --binary -s $SERVER:$PORT --noload --agent={$AGENT1,$AGENT2} --threads=1 --keysize=fb_key --valuesize=fb_value --iadist=fb_ia --update=$UPDATE --depth=128 --measure_connections=32 --qps=$QPS --time=30 >> exp/$UPDATE\_$QPS/leader.log
 
-        scp -r $agent1:~/agent.log exp/$update\_$qps/agent1.log
-        scp -r $agent2:~/agent.log exp/$update\_$qps/agent2.log
-        ssh $agent1 "sudo killall mutilate"
-        ssh $agent2 "sudo killall mutilate"
-        ssh $server "sudo killall memcached"
+        scp -r $AGENT1:~/agent.log exp/$UPDATE\_$QPS/agent1.log
+        scp -r $AGENT2:~/agent.log exp/$UPDATE\_$QPS/agent2.log
+        ssh $AGENT1 "sudo killall mutilate"
+        ssh $AGENT2 "sudo killall mutilate"
+        #ssh $server "sudo killall memcached"
+        sudo kubectl delete -f yaml/memcached.yaml
 }
 
 echo "CREATE TEMP EXP DIR"
